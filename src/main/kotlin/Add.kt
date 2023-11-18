2c4a562aae93d3052ddbf1b72525a2f7f266c753
Ebsuvilla <nelsonokwudili@gmail.com>
2023-11-18 02:44:28 +0100

    Added function with a return type

--- src/main/kotlin/Add.kt
+++ src/main/kotlin/Add.kt
@@ -1,9 +1,17 @@
+import kotlin.Unit as Unit1
+
 fun main (){
     println(sum(2,4))
     println("the total sum: "  + " "+ addition(10, 40))
+    println(printSum(1,1))
+
 }
 
 fun sum (a: Int, b: Int): Int {
     return  a + b
 }
 fun addition (a:Int, b:Int) = a + b
+
+fun printSum(a:Int, b: Int) : Unit1 {
+    println("the sum of $b is ${a + b}")
+}
