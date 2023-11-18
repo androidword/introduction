c1ae4596154c0c428e6d1573f14ba83d5c4e0334
Ebsuvilla <nelsonokwudili@gmail.com>
2023-11-18 02:33:26 +0100

    Initial commit

---
+++ src/main/kotlin/Add.kt
@@ -0,0 +1,9 @@
+fun main (){
+    println(sum(2,4))
+    println("the total sum: "  + " "+ addition(10, 40))
+}
+
+fun sum (a: Int, b: Int): Int {
+    return  a + b
+}
+fun addition (a:Int, b:Int) = a + b
